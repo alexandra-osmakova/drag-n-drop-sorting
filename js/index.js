914b13b956edfe296852ff98ce658a21a15d6bdd
Alexandra Osmakova <alexandra.osmakova@gmail.com>
2019-12-02 18:54:30 +0300

    improve styling, fix sorting and html validity

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@ let list = [
   { id: 2, text: "Dream", icons: "🤡 💅 🙏 🙀" },
   { id: 3, text: "Create", icons: "👩‍🎓 👊 🐥 🌟" }
 ];
-const listItemsWrap = document.getElementById("dropzone");
+const listItemsWrap = document.querySelector('[data-type="dropzone"]');
 let dragStart;
 let dragEnd;
 
 window.onload = function() {
-  list.forEach((el) => createElement(el));
+  list.forEach(el => createElement(el));
   setCurrentListIndex(Array.from(listItemsWrap.children));
 };
 
@@ -19,76 +19,75 @@ function createElement(itemToCreate) {
   listItem.classList.add("content_item");
   listItem.setAttribute("draggable", true);
 
-  listItemInnersList.forEach((el, index) => {
+  listItemInnersList.forEach((el) => {
     const listItemText = document.createElement("span");
     listItemText.innerHTML = itemToCreate[el];
     listItemText.classList.add("content_item-inner");
     if (el === "icons") {
       listItemText.classList.add("content_item-icons");
     }
+    if (el === "id") {
+      listItemText.classList.add('content_item-counter')
+    }
     listItem.appendChild(listItemText);
   });
   listItemsWrap.appendChild(listItem);
 }
 
-listItemsWrap.addEventListener(
-  "dragstart",
-  function(event) {
-    const target = event.target;
-    if (target.hasAttribute("draggable")) {
-      dragStart = target;
-      target.classList.add("dragged");
-    }
+listItemsWrap.addEventListener("dragstart", function(event) {
+  const target = event.target;
+  if (target.hasAttribute("draggable")) {
+    dragStart = target;
+    target.classList.add("dragged");
   }
-);
+});
 
-listItemsWrap.addEventListener(
-  "dragover",
-  function(event) {
-    event.preventDefault();
-  }
-);
+listItemsWrap.addEventListener("dragenter", setDragOverAppearance);
 
-listItemsWrap.addEventListener(
-  "drop",
-  function(event) {
-    event.preventDefault();
-    dragEnd = event.target;
-    if (isInDragZone()) {
-      getItemId(dragStart.id) > getItemId(dragEnd.id)
-        ? dragEnd.parentNode.insertBefore(dragStart, dragEnd)
-        : dragEnd.parentNode.insertBefore(dragStart, dragEnd.nextSibling);
-      const parentChildren = Array.from(listItemsWrap.children);
-      sortCurrentDataList(parentChildren.map(el => getItemId(el.id)));
-      setCurrentListIndex(parentChildren);
-    }
-  }
-);
+listItemsWrap.addEventListener("dragleave", setDragOverAppearance);
 
-listItemsWrap.addEventListener(
-  "dragend",
-  function(event) {
-    dragStart.classList.remove("dragged");
-    dragStart = undefined;
-    dragEnd = undefined;
+listItemsWrap.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
+
+listItemsWrap.addEventListener("drop", function(event) {
+  event.preventDefault();
+  dragEnd = event.target;
+  if (isInDragZone()) {
+    getItemId(dragStart.id) > getItemId(dragEnd.id)
+      ? dragEnd.parentNode.insertBefore(dragStart, dragEnd)
+      : dragEnd.parentNode.insertBefore(dragStart, dragEnd.nextSibling);
+    const parentChildren = Array.from(listItemsWrap.children);
+    sortCurrentDataList(parentChildren.map(el => getItemId(el.id)));
+    setCurrentListIndex(parentChildren);
   }
-);
+});
+
+listItemsWrap.addEventListener("dragend", function() {
+  dragStart.classList.remove("dragged");
+  dragStart = undefined;
+  dragEnd = undefined;
+  Array.from(listItemsWrap.children).forEach(el =>
+    el.classList.remove("dragged_over")
+  );
+});
 
 function getItemId(el) {
   return el.match(/\d/).map(Number)[0];
 }
 
 function isInDragZone() {
-  if (dragEnd.classList.contains('content_item-inner')) {
-    dragEnd = dragEnd.parentNode;
-  }
+  dragEnd = getDraggableItemOnEvent(dragEnd);
   return dragEnd.parentNode === dragStart.parentNode;
 }
 
 function sortCurrentDataList(incomeRow) {
   const currentList = [];
-  incomeRow.forEach(index => {
-    currentList.push(list[index]);
+  const listCounter = Array.from(document.getElementsByClassName('content_item-counter'))
+  incomeRow.forEach((sortParam, index)=> {
+    list[sortParam].id = index + 1;
+    listCounter[index].innerHTML = list[sortParam].id;
+    currentList.push(list[sortParam]);
   });
   list = currentList;
 }
@@ -97,4 +96,18 @@ function setCurrentListIndex(arr) {
   arr.forEach((el, index) => {
     el.id = `item_${index}`;
   });
+}
+
+function setDragOverAppearance(event) {
+  const target = getDraggableItemOnEvent(event.target);
+  if (target.classList.contains("content_item")) {
+    target.classList.toggle("dragged_over");
+  }
+}
+
+function getDraggableItemOnEvent(elem) {
+  if (elem.classList.contains("content_item-inner")) {
+    return elem.parentNode;
+  }
+  return elem;
 }
